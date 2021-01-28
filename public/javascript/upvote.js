d774eb766150f269c1770a57e915bdd28da10941
nrhuizar <nrhuizar@gmail.com>
2021-01-28 15:20:01 -0800

    added login views

---
+++ public/javascript/upvote.js
@@ -0,0 +1,23 @@
+const e = require("express");
+
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+
+    const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
